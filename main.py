89580fd8ddb73bcd7ed6f3f0d92a63954dce1cab
Sergey Nemets <s.nemets@andersenlab.com>
2021-12-02 19:46:15 +0300

    update array initialization

--- main.py
+++ main.py
@@ -3,12 +3,11 @@
 import numpy as np
 from Perceptron import Perceptron
 
-training_inputs = []
-training_inputs.append(np.array([1,1,0]))
-training_inputs.append(np.array([1,0,0]))
-training_inputs.append(np.array([1,0,-1]))
-training_inputs.append(np.array([0,0.5,1]))
-training_inputs.append(np.array([0,5,3]))
+training_inputs = [np.array([1, 1, 0]),
+                   np.array([1, 0, 0]),
+                   np.array([1, 0, -1]),
+                   np.array([0, 0.5, 1]),
+                   np.array([0, 5, 3])]
 
 outputs = np.array([1, 0, 1, 1.5, -3])
 
@@ -28,3 +27,4 @@ if __name__ == '__main__':
     pass
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
+#  https://github.com/nemetsSY/Perceptron
